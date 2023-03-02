4b5abfc01fe1fc73816f89fc096d62657aaf6f65
Nirali Dhorajiya <niralidhorajiya123@gmail.com>
2023-03-02 20:00:47 +0530

    todo done

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_app_second/screens/function_screen.dart';
 import 'package:to_do_app_second/screens/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => TaskData()..loadTasksFromPrefs(),
+      child: MaterialApp(
+        title: 'To Do App',
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: const SplashScreen(),
       ),
-      home: const SplashScreen(),
     );
   }
 }
